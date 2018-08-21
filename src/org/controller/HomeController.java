50d2157939cece9e92b09db2fdfb53bd95eeda93
Josue Robledo <josuerobledo@hotmail.com>
2018-08-21 15:43:37 -0600

    falta agregar las categorias en el array

--- src/org/controller/HomeController.java
+++ src/org/controller/HomeController.java
@@ -10,20 +10,15 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import java.io.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javax.swing.*;
 import org.main.Principal;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Iterator;
-import java.io.IOException;
 import javafx.scene.control.TextArea;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -32,12 +27,6 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
 
-
-
-/**
- *
- * @author programacion
- */
 public class HomeController implements Initializable {
     
     private Stage stage;
@@ -48,6 +37,11 @@ public class HomeController implements Initializable {
     @FXML private TextArea txtSalida;
     FileChooser fileChooser = new FileChooser();
     File selectedFile;
+    
+    String[] Categoria;
+    String[] DescPelicula = new String[4000];;
+    String[] AnioPelicula = new String[4000];
+    
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -74,6 +68,7 @@ public class HomeController implements Initializable {
          selectedFile = fileChooser.showOpenDialog(stage);
         if(selectedFile != null){
             txtFile.setText(selectedFile.getAbsolutePath());
+            btnExport.setDisable(false);
         }
        
        
@@ -97,6 +92,7 @@ try {
             
             Row row;
             
+            int cont=0;
             while (rowIterator.hasNext()){
                 row = rowIterator.next();
                 
@@ -110,47 +106,66 @@ try {
                     String temp;
                     i++;
                     
-                    resultado = celda.toString();
-                     System.out.println("i:  "+i+"///"+resultado);
-                     //String []  bar = resultado.split("::|\\|"); //para remplazar por ! tambien
-                     String []  bar = resultado.split("::");
-                     
-                     for(int j = 0; j<bar.length;j++){
-                         
-                         if(j==0){
-                             
-                             System.out.println("ID:  "+bar[j]);
-                                                          
-                         }else if(j==1){//si es el titulo y si el string contiene (
-                            
-                             if(bar[j].contains("(") == true){
-                                temp = bar[j].replace("(", ":");//si le dejo el ( da error 
-                                String []  split = temp.split(":"); //los separo por :
-                                titulo = split[0]; // para obtener el valor del titulo
-                                titulo = titulo.replace("(","");
-                                titulo = titulo.replace(" ","");
-                                anio = split[1]; //obtengo el segundo valor
-                                anio = anio.replace(")", ""); //lo reemplaso por un valor vacio
-                             }else{
-                                titulo = bar[j].replace(" ","");
-                             }
-                                 
-                            System.out.println("Titulo: "+titulo);//para obtener la fecha
-                            System.out.println("Año: "+anio);//para obtener la fecha
-                            
-                         }else if(j==2){
-                            System.out.println("Categoria: "+bar[j]);//para obtener la fecha
-                         }
-                             
-                                                  
-                    
-                         
-                         
+                    if(i<=1){
+                        
+                        resultado = celda.toString();
+                        System.out.println("***************************************************************************");
+                        System.out.println("Original:   "+resultado);
+                        //String []  bar = resultado.split("::|\\|"); //para remplazar por ! tambien
+                        String []  bar = resultado.split("::");
 
-                         
-                     }
-                     System.out.println(bar + " barr");
+                        for(int j = 0; j<bar.length;j++){
+
+                            if(j==0){
+
+                                System.out.println("ID:  "+bar[j]);
+
+                            }else if(j==1){//si es el titulo y si el string contiene (
+
+                                if(bar[j].contains("(") == true){
+                                    
+                                    temp = bar[j].replace("(", ":");//si le dejo el ( da error 
+                                    String []  split = temp.split(":"); //los separo por :
+                                    
+                                    anio = split[1]; //obtengo el segundo valor
+                                    anio = anio.replace(")", ""); //lo reemplaso por un valor vacio
+                                    
+                                    titulo = split[0]; // para obtener el valor del titulo
+                                    titulo = titulo.replace("(","");
+                                    try {
+                                        Integer.parseInt(anio);
+                                    } catch (NumberFormatException excepcion) {
+                                        
+                                        anio = split[2];
+                                        anio = anio.replace(")", "");
+                                        
+                                        titulo = "";
+
+                                        titulo = split[0] + "("+split[1]; // para obtener el valor del titulo
+                                        //titulo = titulo.replace("(","");
+                                        titulo = titulo.replace(":","(");
+                                    }
+                                    
+                                }
+
+                                System.out.println("Titulo: "+titulo);//para obtener la fecha
+                                DescPelicula[cont] = titulo;
+                                
+                                System.out.println("Año: "+anio);//para obtener la fecha
+                                AnioPelicula[cont] = anio;
+
+                             }else if(j==2){
+                                System.out.println("Categoria: "+bar[j]);//para obtener la fecha
+                                
+                             }
+                        }
+                        
+                    }else {
+                        
+                    }
                 }
+                
+                cont++;
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -159,11 +174,25 @@ try {
     }
     
     
-    
+    public void BuscarCategoria(String Busqueda){
+      
+        //Probar, todavia no funciona
+        for(int i=0;i<Categoria.length;i++){
+            
+            if(Categoria[i].equals(Busqueda)){
+                break; 
+            }else{
+                Categoria[i] = Busqueda;
+            }
+        }
+
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        btnExport.setDisable(true);
     }    
     
 }
